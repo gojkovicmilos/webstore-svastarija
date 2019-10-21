51f604d120178374ed32b0ea936b3d02a88efce4
Milos Gojkovic <gojkovic.milos@outlook.com>
2019-10-21 23:14:50 +0200

    card margins

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -20,8 +20,9 @@ export class ProductComponent implements OnInit {
       this.product.name = actionArray.payload.get('name');
       this.product.price = actionArray.payload.get('price');
       this.product.category = actionArray.payload.get('category');
-      this.product.quantity = actionArray.payload.get('quantity');
+      this.product.quantity = actionArray.payload.get('amount');
       this.product.description = actionArray.payload.get('description');
+      this.product.picture = actionArray.payload.get('pic');
       });
     console.log(this.product);
   }
